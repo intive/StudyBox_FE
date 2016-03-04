e43814ccb032c75c4c6c2cf427369e7debea19cb
amocarski <admocarski@gmail.com>
2016-03-04 15:45:20 +0100

    poprawiona walidacja

--- src/app/registration/registration.controller.js
+++ src/app/registration/registration.controller.js
@@ -2,28 +2,43 @@
   'use strict';
 
 angular
-    .module('studyBoxFe')
-    .controller("RegistrationController", RegistrationController);
+.module('studyBoxFe')
+.run(runMDL)
+.controller('RegistrationController', RegistrationController);
 
-  function RegistrationController() {
-    var vm = this;
-    vm.submit = submit;
-    vm.message = "";
+function runMDL($rootScope, $timeout) {
 
-    vm.user = {
-      nick: "",
-      email: "",
-      password: "",
-      confirmPassword: ""
-    };
+  $rootScope.watcher = $rootScope.$on('$viewContentLoaded', function() {
+      $timeout(function() {
+          componentHandler.upgradeAllRegistered();
+      }, 0);
+  });
+  $rootScope.render = {
+      header: true,
+      aside: true
+  }
+}
 
-    function submit(isValid) {
-      if (isValid) {
-        //testowa informacja potwierdzajaca walidacje formularza
-        vm.message = "wysłano " + vm.user.nick;
-      } else {
-        vm.message = "wymagana poprawa niektórych pól";
-      }
+function RegistrationController($log) {
+  var vm = this;
+  vm.formStatus = '';
+  vm.submit = submit;
+  vm.data = {};
+
+  //wysylanie formularza
+  function submit(isValid) {
+    if (isValid) {
+        vm.formStatus = "Formularz poprawny";
+        $log.info("formularz poprawny dla " +vm.data.login);
+    }else{
+      angular.forEach(vm.registrationForm.$error, function(field) {
+          angular.forEach(field, function(errorField) {
+              errorField.$setTouched();
+          })
+      });
+      vm.formStatus = "Formularz niepoprawny";
+      $log.info("blad formularza");
     }
   }
+}
 })();
