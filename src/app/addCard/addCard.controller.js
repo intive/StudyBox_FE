550a8c44545a59d9f5d372d613f6507e77a927b2
Patryk Janik <pumanitro@gmail.com>
2016-03-09 22:10:09 +0100

    Plus button zmienia się Responsywnie

--- src/app/addCard/addCard.controller.js
+++ src/app/addCard/addCard.controller.js
@@ -3,12 +3,15 @@
 
   angular
     .module('studyBoxFe')
+    .directive('elastic', ElasticDirective)
     .controller('AddCardController', AddCardController);
 
 
   /** @ngInject */
   function AddCardController($stateParams, $state, $window) {
     var vm = this;
+    vm.id = $stateParams.id;
+    vm.innerHeight = {height:$window.innerHeight+ 'px'};
 
     vm.getDecks = function () {
       //service will be used in future
@@ -22,6 +25,21 @@
 
     vm.categories = vm.getDecks();
 
+  }
+
+  function ElasticDirective() {
+    return {
+      restrict: 'A',
+      link: function($scope, element) {
+        $scope.initialHeight = $scope.initialHeight || element[0].style.height;
+        var resize = function() {
+          element[0].style.height = $scope.initialHeight;
+          element[0].style.height = "" + element[0].scrollHeight + "px";
+        };
+        element.on("input change", resize);
+        $timeout(resize, 0);
+      }
+    };
   }
 
 })();
