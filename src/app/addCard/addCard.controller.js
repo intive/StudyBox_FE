e2820aa45ea2cbc2646957094f7b565c3421307b
Marcin Werkowski <marcin.werkowski@blstream.com>
2016-03-10 16:05:42 +0100

    usuniecie mockowych danych z controllera

--- src/app/addCard/addCard.controller.js
+++ src/app/addCard/addCard.controller.js
@@ -11,18 +11,6 @@
     var vm = this;
     vm.id = $stateParams.id;
 
-    vm.getDecks = function () {
-      //service will be used in future
-
-      return[
-        {id: 0, name: 'deck_0'},
-        {id: 12, name: 'deck_1'},
-        {id: 23, name: 'deck_2'},
-        {id: 34, name: 'deck_3'}];
-    };
-
-    vm.categories = vm.getDecks();
-
     var toggleStatus = true;
 
     vm.toggleButton = function ()
