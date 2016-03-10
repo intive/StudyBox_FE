b02054de52528c6f4518519448b8edde2797367c
Patryk Janik <pumanitro@gmail.com>
2016-03-10 02:31:05 +0100

    Add Card

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,16 +8,18 @@
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-        .state('home', {
-            url: '/',
-            template: 'Study Box Front-end'
-        })
-        .state('addCard', {
-            url: '/addCard',
-            controller: 'AddCardController',
-            controllerAs: 'AddCardController',
-            templateUrl: 'app/addCard/addCard.html'
-        })
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/login/landing.html',
+        controller: 'HomeController',
+        controllerAs: 'home'
+      })
+      .state('addCard', {
+        url: '/addCard',
+        controller: 'AddCardController',
+        controllerAs: 'AddCardController',
+        templateUrl: 'app/addCard/addCard.html'
+      })
       .state('decks', {
         parent: 'home',
         url: 'decks',
@@ -38,20 +40,14 @@
         controller: 'RegistrationController',
         controllerAs: 'registration'
       })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/login/login.html',
-        controller: 'LoginController',
-        controllerAs: 'login'
-      })
       .state('landing', {
         url: '/landing',
-        templateUrl: 'app/landing/landing.html',
+        templateUrl: 'app/login/landing.html',
         controller: 'LandingController',
         controllerAs: 'landing'
       });
 
-    $urlRouterProvider.otherwise('/decks');
+    $urlRouterProvider.otherwise('/');
   }
 
 })();
