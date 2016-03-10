06f5d933ec3e86b9484f41c93fd0b8f3e556662c
Jakub Ligęza <dagins92@gmail.com>
2016-03-10 10:59:15 +0100

    Proxy.

--- gulp/server.js
+++ gulp/server.js
@@ -33,7 +33,13 @@ function browserSyncInit(baseDir, browser) {
    *
    * For more details and option, https://github.com/chimurai/http-proxy-middleware/blob/v0.9.0/README.md
    */
-  // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', changeOrigin: true});
+  server.middleware = proxyMiddleware('/api', {
+    target: 'http://private-anon-0160c33ac-studybox.apiary-mock.com',
+    pathRewrite: {
+        '^/api' : ''
+    },
+    changeOrigin: true
+  });
 
   browserSync.instance = browserSync.init({
     startPath: '/',
