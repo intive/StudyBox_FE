ffe03362cd05ce3e7056efa596603dfda1c9dce6
Jarek-S <jareks@naszepolice.pl>
2016-03-12 15:22:28 +0100

    poprawiona konfiguracja translatora

--- src/app/index.translator.js
+++ src/app/index.translator.js
@@ -12,6 +12,7 @@
       prefix: '/assets/i18n/',
       suffix: '/common.json'
     });
+    $translateProvider.useSanitizeValueStrategy('sanitize');
     $translateProvider.preferredLanguage('pl_PL');
   }
 
