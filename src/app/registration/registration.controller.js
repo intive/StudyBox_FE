75f62b719d7e250eee45e1f773ab966dbfe5ef0a
amocarski <admocarski@gmail.com>
2016-03-15 17:18:05 +0100

    sprawdzanie czy user jest online

--- src/app/registration/registration.controller.js
+++ src/app/registration/registration.controller.js
@@ -6,19 +6,20 @@ angular
 .controller('RegistrationController', RegistrationController);
 
 
-function RegistrationController($log) {
+function RegistrationController($log, $mdDialog) {
   var vm = this;
   vm.formStatus = '';
   vm.submit = submit;
   vm.reset = reset;
+  vm.showAlert = showAlert;
   vm.data = {};
   vm.imagePath = "assets/images/StudyBoxLogo_xx.png";
 
   //wysylanie formularza
   function submit(isValid) {
     if (isValid) {
-        vm.formStatus = "Formularz poprawny";
-        $log.info("formularz poprawny dla " +vm.data.login);
+      vm.formStatus = "Formularz poprawny";
+      $log.info("formularz poprawny dla" +vm.data.email+".");
     }else{
       vm.formStatus = "Formularz niepoprawny";
       $log.info("blad formularza");
@@ -27,6 +28,21 @@ function RegistrationController($log) {
   //anulowanie formularza
   function reset(){
     vm.data = {};
+  }
+
+  function showAlert(ev, online) {
+    if(!online){
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Uwaga!')
+          .textContent('Utraciłeś połączenie z internetem, twoja rejestracja się nie powiodła. Spróbuj później.')
+          .ariaLabel('Alert Dialog Demo')
+          .ok('Rozumiem')
+          .targetEvent(ev)
+      );
+    }
   }
 }
 })();
