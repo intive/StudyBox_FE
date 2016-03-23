75baf06cc7321ebb99d75e5b154eb08cdf4d131f
Patryk Janik <pumanitro@gmail.com>
2016-03-23 22:53:24 +0100

    Próba podpięcia do APi

--- src/app/addCard/addCard.controller.js
+++ src/app/addCard/addCard.controller.js
@@ -25,7 +25,30 @@
       vm.toggleStatus = !vm.toggleStatus;
     };
 
+    vm.submitCard = function ()
+    {
+      $window.alert(angular.element($document[0].querySelector("input[name='deckForm']")).css("display", "none"));
 
+      //Jeżeli pola nie są puste
+      if(vm.question!=null && vm.answer!=null)
+      {
+        if($stateParams.cardId == null)
+        {
+          //Dodawanie
+          BackendService.createFlashcard(vm.question, vm.answer);
+        }
+        else
+        {
+          //Edycja
+          BackendService.updateFlashcard($stateParams.cardId, vm.question, vm.answer);
+        }
+      }
+
+      //if nowy name
+      //$stateParams.deckId
+
+
+    };
 
   }
 
