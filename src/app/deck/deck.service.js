b076ce0cb41ffd3e3bf464ccb021e68f88defd60
Marcin Werkowski <marcin.werkowski@blstream.com>
2016-03-24 12:30:25 +0100

    # dodanie obj karty do serwisu

--- src/app/deck/deck.service.js
+++ src/app/deck/deck.service.js
@@ -8,13 +8,25 @@
   /** @ngInject */
   function DeckService() {
     var deckName;
+    var deckObj;
     return {
-      setDeckName: function (name) {
-        deckName = name
-      },
-      getDeckName: function () {
-        return deckName
-      }
+      setDeckName: setDeckName,
+      getDeckName: getDeckName,
+      setDeckObj: setDeckObj,
+      getDeckObj: getDeckObj
+    };
+
+    function setDeckName(name) {
+      deckName = name
+    }
+    function getDeckName() {
+      return deckName
+    }
+    function setDeckObj(obj) {
+      deckObj = obj
+    }
+    function getDeckObj() {
+      return deckObj
     }
   }
 })();
