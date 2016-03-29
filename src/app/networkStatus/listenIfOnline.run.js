194b483d974de4c431c56be5a5ceadd66c9fd2b1
amocarski <admocarski@gmail.com>
2016-03-29 19:03:53 +0200

    nasluchiwanie stanu sieci - globalnie

---
+++ src/app/networkStatus/listenIfOnline.run.js
@@ -0,0 +1,51 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('studyBoxFe')
+    .run(ifOnline);
+
+/** @ngInject */
+  function ifOnline($rootScope, $window, $mdDialog, $document) {
+    $rootScope.online = $window.navigator.onLine;
+    if (!$rootScope.online) {
+        showOfflineAlert();
+        // alert('Offline! pierwszy');
+    }
+
+    function showOfflineAlert() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element($document[0].querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Uwaga!')
+          .textContent('Utraciłeś połączenie z internetem. Spróbuj później.')
+          .ariaLabel('Alert Dialog')
+          .ok('Rozumiem')
+      );
+    }
+
+    function showOnlineAlert() {
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element($document[0].querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('Uwaga!')
+          .textContent('Odzyskałeś połączenie z internetem, możesz kontynuować.')
+          .ariaLabel('Alert Dialog')
+          .ok('Rozumiem')
+      );
+    }
+
+    $window.addEventListener("offline", function() {
+      showOfflineAlert();
+      // alert('Offline!');
+    }, false);
+
+    $window.addEventListener("online", function() {
+        showOnlineAlert();
+        // alert('Online!');
+    }, false);
+  }
+
+})();
