5cebd02affbd4c433baf7135eafe4d299eb458e2
Jakub Ligęza <dagins92@gmail.com>
2016-04-12 15:46:15 +0200

    Poprawienie deck-preview plus refactoring.

--- src/app/navbar/outsideClick.directive.js
+++ src/app/navbar/outsideClick.directive.js
@@ -7,42 +7,34 @@
 
   /** @ngInject */
   function outsideClick($document) {
-return {
-  restrict:'E',
+    return {
+      restrict:'E',
 
-                link: function (scope, element) {
-                    $document.on("click", function () {
-                        if(event.target.id != 'searchButton'){
-                          if(event.target.id != 'searchButton2'){
-                            if(event.target.id != 'searchButton3'){
-                         angular.element($document[0].querySelector('#searchAutocomplete')).removeClass('showUp');
-                         angular.element($document[0].querySelector('#searchAutocomplete')).addClass('searchForm');
-                         angular.element($document[0].querySelector('#searchButton2')).removeClass('ng-hide');
-                         angular.element($document[0].querySelector('#searchButton3')).addClass('ng-hide');
-                         angular.element($document[0].querySelector('#searchButton')).removeClass('darkButton');
-                       }
-                       }
-                        }
-                    });
+      link: function (scope, element) {
+        $document.on("click", function () {
+          if(event.target.id != 'searchButton'){
+            if(event.target.id != 'searchButton2'){
+              if(event.target.id != 'searchButton3'){
+                angular.element($document[0].querySelector('#searchAutocomplete')).removeClass('showUp');
+                angular.element($document[0].querySelector('#searchAutocomplete')).addClass('searchForm');
+                angular.element($document[0].querySelector('#searchButton2')).removeClass('ng-hide');
+                angular.element($document[0].querySelector('#searchButton3')).addClass('ng-hide');
+                angular.element($document[0].querySelector('#searchButton')).removeClass('darkButton');
+              }
+            }
+          }
+        });
 
-                  element.on("click", function (evt) {
+        element.on("click", function (evt) {
+          evt.stopPropagation();
+        });
 
-                    evt.stopPropagation();
+        var mdv = angular.element(document).find("md-virtual-repeat-container");
 
-                  })
-
-                   var mdv = angular.element(document).find("md-virtual-repeat-container");
-
-                  mdv.on("click", function (evt) {
-
-                    evt.stopPropagation();
-
-                  })
-
-
-                }
-
-}
-
-}
+        mdv.on("click", function (evt) {
+          evt.stopPropagation();
+        });
+      }
+    };
+  }
 })();
