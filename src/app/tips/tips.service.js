1f0af8387b95c6ad5737839da1da3dbb95a2a231
Patryk Janik <pumanitro@gmail.com>
2016-04-13 23:14:47 +0200

    Stworzono funkcję getAllTips.

--- src/app/tips/tips.service.js
+++ src/app/tips/tips.service.js
@@ -3,6 +3,7 @@
  *
  *  createNewTip(deckId,flashCardId,content) - tworzy nową podpowiedź w decku o podanym ID, dla karty o podanym ID o danej treści. Zwraca obiekt Tip z jego id i contentem.
  *  getTipById(deckId,flashcardId,tipId) - pobiera nam dane podpowiedzi z wybranego decka i karty po jej ID. Zwraca obiekt Tip z jego id i contentem.
+ *  getAllTips(deckId,flashcardId) - pobiera wszystkie podpowiedzi z danego decka, z danej karty. Zwraca tablicę obiektów typu Tip.
  *
  *  Tip [klasa]:
  *  > pola:
@@ -72,6 +73,32 @@
           tip.id = response.data.id;
           tip.content = response.data.prompt;
           return tip;
+        },
+        function error(response) {
+          return $q.reject(response.data);
+        }
+      );
+    }
+
+    function getAllTips(deckId,flashcardId)
+    {
+      if(angular.isUndefined(deckId) ) show_error('Must specify deck id');
+      if(angular.isUndefined(flashcardId) ) show_error('Must specify flash card id');
+
+      var method = 'GET';
+      var url = '/api/decks/'+deckId+'/flashcards/'+flashcardId+'/tips';
+
+      return $http({method: method, url: url})
+        .then(
+        function success(response) {
+          var tips = [];
+          for(var i = 0; i < response.data.length; i++) {
+            var tip = new Tip();
+            tip.id = response.data[i].id;
+            tip.content = response.data[i].prompt;
+            tips.push(tip);
+          }
+          return tips;
         },
         function error(response) {
           return $q.reject(response.data);
