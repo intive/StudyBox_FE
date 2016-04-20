1e4be3292cb890e80cf40aa7814300e6282d399f
amocarski <admocarski@gmail.com>
2016-04-20 18:11:16 +0200

    wyswietlanie podpowiedzi 331

--- src/app/deck/deck-preview.controller.js
+++ src/app/deck/deck-preview.controller.js
@@ -6,7 +6,7 @@
     .controller('DeckPreviewController', DeckPreviewController);
 
   /** @ngInject */
-  function DeckPreviewController($stateParams, $state, BackendService, $log, DeckService) {
+  function DeckPreviewController($stateParams, $state, BackendService, $log, DeckService, $mdDialog, $mdMedia, $document, $scope) {
     var vm = this;
     vm.deckId = $stateParams.deckId;
     vm.selectedDeck = new BackendService.Deck();
@@ -14,6 +14,8 @@
     vm.clear = clear;
     vm.getDecks = getDecks;
     vm.selectDeck = selectDeck;
+    vm.hintsListDialog = hintsListDialog;
+    vm.cancelDialog = cancelDialog;
 
     vm.access = $stateParams.access;
 
@@ -116,6 +118,24 @@
       pickUpCard($stateParams.cardId);
     }
     initDeck($stateParams.deckId);
+
+  function hintsListDialog(ev) {
+    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+    $mdDialog.show({
+      controller: DeckPreviewController,
+      templateUrl: 'app/deck/hintsList.html',
+      parent: angular.element($document.body),
+      targetEvent: ev,
+      scope: $scope,
+      preserveScope: true,
+      clickOutsideToClose:true,
+      fullscreen: useFullScreen
+    });
+  }
+
+  function cancelDialog(){
+    $mdDialog.hide();
+  }
 
   }
 })();
