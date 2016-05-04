0a669eea61c8f6cff7be4b6f499a4129e90a2861
Patryk Janik <pumanitro@gmail.com>
2016-05-04 22:00:25 +0200

    Zmieniono prompt na essence i postawiono spacje po przecinkach.

--- src/app/backend/tip.factory.js
+++ src/app/backend/tip.factory.js
@@ -10,7 +10,7 @@
     function Tip() {
       // fields
       this.id = null;
-      this.prompt = null;
+      this.essence = null;
       this.deckId = null;
       this.flashcardId = null;
 
@@ -25,16 +25,16 @@
         throw message;
       }
 
-      function update(new_prompt,difficult)
+      function update(new_essence, difficult)
       {
-        if(!new_prompt) throw_error('Must specify content of tip');
+        if(!new_essence) throw_error('Must specify content of tip');
 
         //chwilowe rozwiązanie
         difficult = 0;
 
         var method = 'PUT';
         var url = '/api/decks/'+this.deckId+'/flashcards/'+this.flashcardId+'/tips/'+this.id;
-        var data = {essence: new_prompt,difficult: difficult};
+        var data = {essence: new_essence, difficult: difficult};
 
         return $http({method: method, url: url, data: data})
           .then(
