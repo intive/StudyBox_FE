37c1df25c985a66a794bf02e3968f2427620fe10
Patryk Janik <pumanitro@gmail.com>
2016-05-10 22:32:57 +0200

    Problem z label'em w haśle rozwiązany.

--- src/app/login/login.controller.js
+++ src/app/login/login.controller.js
@@ -3,7 +3,39 @@
 
   angular
   .module('login')
-  .controller('LoginController', LoginController);
+  .controller('LoginController', LoginController)
+  .config(function($provide)
+  {
+    $provide.decorator('mdInputContainerDirective', function($delegate, $interval) {
+      var directive = $delegate[0];
+
+      directive.compile = function() {
+        return {
+          post: function($scope, element, attr, ctrl) {
+            var interval;
+            var count = 0;
+
+            if (ctrl.input[0].type === 'password') {
+              interval = $interval(function() {
+                if (count > 10) {
+                  $interval.cancel(interval);
+                }
+
+                if (ctrl.input.parent()[0].querySelector('input:-webkit-autofill')) {
+                  ctrl.element.addClass('md-input-has-value');
+                  $interval.cancel(interval);
+                }
+
+                count++;
+              }, 25);
+            }
+          }
+        };
+      };
+
+      return $delegate;
+    });
+  });
 
   function LoginController($state, LoginService, $stateParams, LoginHelperService, $log, $q) {
     var vm = this;
